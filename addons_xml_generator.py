cbc37a0e45610fcfeb84480ad702ac9f1a7e2ba8
Spikers2 <131625903+Spikers2@users.noreply.github.com>
2024-01-27 14:04:14 -0500

    update

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -1,7 +1,7 @@
 """ addons.xml generator """
 
 import os
-import md5
+import hashlib
 
 
 class Generator:
@@ -15,7 +15,7 @@ class Generator:
         self._generate_addons_file()
         self._generate_md5_file()
         # notify user
-        print "Finished updating addons xml and md5 files"
+        print("Finished updating addons xml and md5 files")
 
     def _generate_addons_file( self ):
         # addon list
@@ -38,12 +38,12 @@ class Generator:
                     # skip encoding format line
                     if ( line.find( "<?xml" ) >= 0 ): continue
                     # add line
-                    addon_xml += unicode( line.rstrip() + "\n", "UTF-8" )
+                    addon_xml += str(line.rstrip() + "\n", "UTF-8")
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
-            except Exception, e:
+            except Exception as e:
                 # missing or poorly formatted addon.xml
-                print "Excluding %s for %s" % ( _path, e, )
+                print("Excluding %s for %s" % ( _path, e, ))
         # clean and add closing tag
         addons_xml = addons_xml.strip() + u"\n</addons>\n"
         # save file
@@ -52,20 +52,20 @@ class Generator:
     def _generate_md5_file( self ):
         try:
             # create a new md5 hash
-            m = md5.new( open( "addons.xml" ).read() ).hexdigest()
+            m = hashlib.md5(open("addons.xml").read()).hexdigest()
             # save file
-            self._save_file( m, file="addons.xml.md5" )
-        except Exception, e:
+            self._save_file(m, file="addons.xml.md5")
+        except Exception as e:
             # oops
-            print "An error occurred creating addons.xml.md5 file!\n%s" % ( e, )
+            print("An error occurred creating addons.xml.md5 file!\n%s" % ( e, ))
 
     def _save_file( self, data, file ):
         try:
             # write data to the file
             open( file, "w" ).write( data )
-        except Exception, e:
+        except Exception as e:
             # oops
-            print "An error occurred saving %s file!\n%s" % ( file, e, )
+            print("An error occurred saving %s file!\n%s" % ( file, e, ))
 
 
 if ( __name__ == "__main__" ):
